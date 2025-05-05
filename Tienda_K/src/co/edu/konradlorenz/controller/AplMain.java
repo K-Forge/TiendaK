f392ad930d867db0dc6b4133164477655c79ce02
KamiroDark <prietocamilo21@gmail.com>
2025-05-04 23:19:18 -0500

    NEW >>> Inicia el código en Java
    Se agregan las clases principales y se crea el proyecto para iniciar a codificar.

---
+++ Tienda_K/src/co/edu/konradlorenz/controller/AplMain.java
@@ -0,0 +1,16 @@
+package co.edu.konradlorenz.controller;
+
+/**
+ * @author Alejandra Duran
+ * @author Camilo Prieto
+ * @author Mike
+ */
+public class AplMain {
+
+    
+    public static void main(String[] args) {
+        Control obj = new Control();
+        obj.run();
+    }//Cierre Main
+    
+}//Cierre AplMain Class
---
+++ Tienda_K/src/co/edu/konradlorenz/controller/Control.java
@@ -0,0 +1,10 @@
+package co.edu.konradlorenz.controller;
+
+
+public class Control {
+    
+    public void run(){
+        
+    }
+    
+}//Cierre Control
